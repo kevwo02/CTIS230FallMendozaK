84735a847bcc93f3d2c98971c2ece49b7e1bb87d
kevwo02 <mendozakm@guilford.edu>
2022-08-30 05:54:13 -0400

    adding buttons and text fields

--- jsKevin.js
+++ jsKevin.js
@@ -10,7 +10,45 @@ function onClick() {
 
 myitem.addEventListener("mouseout", onmouseout);
 
+// use the first in the ordered list, I reverted the color and size
+//to its original view
 function onmouseout(){
     myitem.style.color="cornflowerblue";
     myitem.style.fontSize="2vw";
 }
+
+//will add the button to the second in the list
+
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+
+// this button makes changes to 3 item on the list , 
+//making it orange and increasing the font
+thebutton2 = document.getElementById("thebutton2");  
+otheritem2 = document.getElementById("test2");
+
+thebutton2.addEventListener("click", onButtonClick2);
+
+
+function onButtonClick2() {  
+    otheritem2.style.color = "orange";  
+    otheritem2.style.fontSize="14pt";
+}
+
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
+
+
+
+
